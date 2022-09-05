4ac60037a8ce483586b2ee0ee1b913c1ec6b42f8
노정은 <wjddms4107@naver.com>
2022-09-05 17:47:50 +0900

    Modify: area detail 페이지 refactoring v1.0

--- src/pages/Area/components/AreaDetailLineChart.js
+++ src/pages/Area/components/AreaDetailLineChart.js
@@ -7,15 +7,10 @@ import {
   CartesianGrid,
   ResponsiveContainer,
 } from 'recharts';
-import { customizeLine } from 'utils/functions/area/areaDetail';
 
 export const AareaDetailLineChart = ({ areaLineChartData }) => {
-  const isData = areaLineChartData.length !== 0;
-  if (!isData) return <div>로딩중입니다.</div>;
-
   const CustomizedLabel = props => {
     const { x, y, value, index } = props;
-
     return (
       <text x={x} y={y} dy={-20} fontSize={18} textAnchor="middle">
         {index === 0 ? null : value}
@@ -23,13 +18,14 @@ export const AareaDetailLineChart = ({ areaLineChartData }) => {
     );
   };
 
+  if (!areaLineChartData) return <div>로딩중입니다.</div>;
   return (
     <div className="pl-3">
       <ResponsiveContainer width="95%" height={400} className="pt-3">
         <LineChart
           width={500}
           height={200}
-          data={customizeLine(areaLineChartData, areaLineChartData[0].progress)}
+          data={areaLineChartData}
           syncId="anyId"
           margin={{
             top: 20,
--- src/utils/functions/area/areaDetail.js
+++ src/utils/functions/area/areaDetail.js
@@ -1,3 +1,9 @@
+/**
+ * 공정률 Line Chart 데이터 배열 맨 앞에 객체 추가하는 함수
+ * @param {*} LineData
+ * @param {*} value
+ * @returns 맨 앞에 객체 하나가 추가된 최종 공정률 Line chart 데이터 배열
+ */
 export function customizeLine(LineData, value) {
   LineData.unshift({ day: null, progress: value });
   return LineData;
